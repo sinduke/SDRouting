46f60470dc8ceb5a7e72c76a30d855137bd51f73
sinduke <sinduke@outlook.com>
2026-04-10 20:39:39 +0800

    将 AnyAppAlert 和 AppButton 的相关属性和方法修改为公共，以增强可访问性

--- Sources/SDRouting/Models/AnyAppAlert.swift
+++ Sources/SDRouting/Models/AnyAppAlert.swift
@@ -1,49 +1,49 @@
 import SwiftUI
 
 public struct AnyAppAlert: Identifiable {
-  enum Style { case alert, confirm }
+  public enum Style { case alert, confirm }
 
-  struct AppButton: Identifiable {
-    let id = UUID()
-    var title: String
-    var role: ButtonRole?
-      var action: (() -> Void)?
+  public struct AppButton: Identifiable {
+    public let id = UUID()
+    public var title: String
+    public var role: ButtonRole?
+    public var action: (() -> Void)?
 
-    static func ok(_ title: String = "OK", action: (() -> Void)? = nil) -> Self {
+    public static func ok(_ title: String = "OK", action: (() -> Void)? = nil) -> Self {
       .init(title: title, role: .cancel, action: action)
     }
 
-    static func cancel(_ title: String = "Cancel", action: (() -> Void)? = nil) -> Self {
+    public static func cancel(_ title: String = "Cancel", action: (() -> Void)? = nil) -> Self {
       .init(title: title, role: .cancel, action: action)
     }
 
-    static func destructive(_ title: String, action: (() -> Void)? = nil) -> Self {
+    public static func destructive(_ title: String, action: (() -> Void)? = nil) -> Self {
       .init(title: title, role: .destructive, action: action)
     }
   }
 
   public let id = UUID()
-  var style: Style = .alert
-  var title: String
-  var message: String
-  var buttons: [AppButton] = []
+  public var style: Style = .alert
+  public var title: String
+  public var message: String
+  public var buttons: [AppButton] = []
 
-  init(style: Style = .alert, title: String, message: String, buttons: [AppButton] = []) {
+  public init(style: Style = .alert, title: String, message: String, buttons: [AppButton] = []) {
     self.style = style
     self.title = title
     self.message = message
     self.buttons = buttons.isEmpty ? [.ok()] : buttons
   }
 
-  init(error: Error) {
+  public init(error: Error) {
     self.init(style: .alert, title: "Error", message: error.localizedDescription, buttons: [.ok()])
   }
 
-  static func ok(title: String = "Alert", message: String) -> AnyAppAlert {
+  public static func ok(title: String = "Alert", message: String) -> AnyAppAlert {
     .init(style: .alert, title: title, message: message, buttons: [.ok()])
   }
 
-  static func confirm(
+  public static func confirm(
     title: String = "Confirm",
     message: String,
     confirmTitle: String = "OK",
