79ffc2add5de3fd159f30d305f5a193cc344211f
sinduke <sinduke@outlook.com>
2026-04-10 17:57:23 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SDRouting",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SDRouting",
+            targets: ["SDRouting"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SDRouting"
+        ),
+        .testTarget(
+            name: "SDRoutingTests",
+            dependencies: ["SDRouting"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
