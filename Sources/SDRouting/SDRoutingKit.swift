9cc5e4e89b8c6a720c9d534c03f9e608345da165
sinduke <sinduke@outlook.com>
2026-04-12 13:57:28 +0800

    新增 SDRoutingKit 枚举，定义当前包版本

--- Sources/SDRouting/SDRoutingKit.swift
+++ Sources/SDRouting/SDRoutingKit.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public enum SDRouting {
+public enum SDRoutingKit {
 	/// The current package version.
 	public static let version = "0.0.1"
 }
